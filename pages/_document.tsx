b034f2151a0e20c5d81c9c8ca91dbc719115b519
Sam Peterson <116111548+sampeterson999@users.noreply.github.com>
2023-09-19 11:03:57 -0400

    change favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -31,10 +31,7 @@ export default function Document() {
           href="https://www.yourbook.recipes/"
         />
         <meta charSet="UTF-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1"
-        />
+
         <link
           rel="icon"
           href="/favicon.ico"
